64bc9a31ca28aa0932414b733f7711e76b3eaf46
Sergio-rsd <sergsa2001@mail.ru>
2022-02-14 02:36:35 +0300

    Заработало

--- app/src/main/java/ru/gb/lesson12/data/InMemoryRepoImpl.java
+++ app/src/main/java/ru/gb/lesson12/data/InMemoryRepoImpl.java
@@ -1,12 +1,21 @@
 package ru.gb.lesson12.data;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryRepoImpl implements Repo {
 
+    private static final String NOTES_SAVE = "NOTES_SAVE";
     private ArrayList<Note> notes = new ArrayList<>();
     private int counter = 0;
+
+    private static Gson gson = new Gson();
+//    private static SharedPreferences prefs;
 
     // singleton для базы
     private static InMemoryRepoImpl repo;
@@ -18,8 +27,41 @@ public class InMemoryRepoImpl implements Repo {
         return repo;
     }
 
+    @Override
+    public List<Note> readPref(String notesString) {
+//        notes = gson.fromJson(notesString, new TypeToken<List<Note>>() {}.getType());
+//        notes = gson.fromJson(notesString, new TypeToken<List<Note>>() {}.getType());
+        Type type =  new TypeToken<List<Note>>() {}.getType();
+        notes = new GsonBuilder().create().fromJson(notesString, type);
+
+/*        StringBuilder builder = new StringBuilder();
+        for (Note n : notes) {
+            builder.append(n.getId());
+            builder.append(n.getTitle());
+            builder.append(n.getDescription());
+            builder.append(n.getInterest());
+            builder.append(n.getDataPerformance());
+
+        }*/
+//        notesString
+
+        return notes;
+    }
+
+    @Override
+    public void writePref(List<Note> notes) {
+
+        String listNotes = gson.toJson(notes);
+        SharedPref.write(NOTES_SAVE, listNotes);
+//        prefs
+//                .edit()
+//                .putString(NOTES_SAVE, listNotes)
+//                .apply();
+    }
 
     private InMemoryRepoImpl() {
+
+ /*
         create(new Note("Title 1", "Description 1"));
         create(new Note("Title 2", "Description 2"));
         create(new Note("Title 3", "Description 3"));
@@ -36,18 +78,15 @@ public class InMemoryRepoImpl implements Repo {
         create(new Note("Title 14", "Description 14"));
         create(new Note("Title 15", "Description 15"));
         create(new Note("Title 16", "Description 16"));
+  */
+
     }
-
-
-
 
     @Override
     public int create(Note note) {
         int id = counter++;
         note.setId(id);
         notes.add(note);
-//        getAll();
-
         return id;
     }
 
@@ -68,7 +107,6 @@ public class InMemoryRepoImpl implements Repo {
                 break;
             }
         }
-//        getAll();
     }
 
     @Override
